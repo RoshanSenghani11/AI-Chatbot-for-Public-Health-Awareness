7e0fd15fd6a9a2bbd3b894906ce2f85598f171b6
Roshan Senghani <topgamerz2106@gmail.com>
2025-09-08 20:07:43 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI project',
+    version = '0.0.1',
+    author = 'Roshan Patel',
+    author_email = 'topgamerz2106@gmail.com',
+    packages = find_packages(),
+    install_requires = [] 
+)
